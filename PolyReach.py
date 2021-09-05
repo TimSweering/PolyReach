2667897844b21b1b6e6edeb7328a7c4cb4978507
TimSweering <T.H.J.Sweering@student.tudelft.nl>
2021-09-05 20:43:51 +0200

    requirements.txt update + parameter file

--- PolyReach.py
+++ PolyReach.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import sys
 sys.path.append("..")
 # print('---------\n%s\n---------' % sys.argv[1])
-# file_in = 'parameter_files/param.json'
+# file_in = 'parameter_files/van_der_pol.json'
 # read_parameter_file(sys.argv[1])
 read_parameter_file(sys.argv[1])
 plt.show()
